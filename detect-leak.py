adec74de152952165c82d9707dbc14dba74caa99
soumyabrata23130 <soumyabrata23130@gmail.com>
2025-08-31 19:25:34 +0000

    formatting

--- detect-leak.py
+++ detect-leak.py
@@ -10,18 +10,23 @@ import random
 import sqlite3
 import time
 
+
 # age of pipeline
 age = random.uniform(0, 10)
+
 if random.random() < 0.2:
   age += random.uniform(4, 16)
+
 if age > 10:
   print(f"[ALERT] Old pipeline! Age = {age:.0f} years")
   print("Sending alert to maintenance team...")
 else:
   print(f"Age of pipeline = {age:.0f} years")
 
+
 # pH of water
 ph = random.uniform(2, 12)
+
 if ph < 6:
   print(f"[ALERT] Acidic water! pH = {ph:.2f}")
   print("Sending alert to water supply team...")
@@ -31,19 +36,24 @@ elif ph > 8:
 else:
   print(f"Normal water. pH = {ph:.2f}")
 
+
 sample_range = 50 # sample range for this program, in deciseconds
+
 
 # connect to SQLite database
 conn = sqlite3.connect("pipeline-data.db")
 cursor = conn.cursor()
+
 cursor.execute("DROP TABLE IF EXISTS PIPELINE")
 cursor.execute("CREATE TABLE IF NOT EXISTS PIPELINE(TIME REAL, FLOW REAL, PRESSURE REAL, LEAKED TEXT)")
+
 
 # initialize multiple lists
 flow = np.random.uniform(5, 10, sample_range) # in L/s
 pressure = np.random.uniform(200, 300, sample_range) # in kPa
-leak_indices = []
 timestamps = np.arange(0, sample_range/10, 0.1) # timestamps from 0 to sample range, in seconds
+leak_indices = []
+
 
 # to print multiple leak alerts and store both normal and leaked data
 for i in range(sample_range):
@@ -66,12 +76,13 @@ for i in range(sample_range):
   cursor.execute(f"INSERT INTO PIPELINE VALUES ({timestamps[i]:.1f}, {flow[i]:.2f}, {pressure[i]:.2f}, '{leaked_status}')")
 
 
-# Lists for leaks
+# lists for leaks
 leak_timestamps = [timestamps[i] for i in leak_indices]
 leak_flows = [flow[i] for i in leak_indices]
 leak_pressures = [pressure[i] for i in leak_indices]
 
-# Print database
+
+# print database
 print() # to create gap
 print("Pipeline data:")
 print("(time (s), flow (L/s), pressure (kPa), leaked)")
@@ -79,7 +90,12 @@ cursor.execute("SELECT * FROM PIPELINE")
 for row in cursor.fetchall():
   print(row)
 
-# Visualization
+# commit changes and close connection
+conn.commit()
+conn.close()
+
+
+# visualization
 fig, ax = plt.subplots(2) # two subplots
 
 ax[0].set_title("Sensor Data Simulation") # title
@@ -96,15 +112,11 @@ ax[1].set_xlabel("Time (s)")
 ax[1].set_ylabel("Pressure (kPa)")
 ax[1].grid(True)
 
-# Mark leaks
+# mark leaks
 if leak_indices:
   ax[0].scatter(leak_timestamps, leak_flows, color="red", marker="o", s=100, label="Detected Leak")
   ax[1].scatter(leak_timestamps, leak_pressures, color="red", marker="o", s=100, label="Detected Leak")
 
-# Display plot
+# display plot
 plt.tight_layout()
 plt.show()
-
-# commit changes and close connection
-conn.commit()
-conn.close()
